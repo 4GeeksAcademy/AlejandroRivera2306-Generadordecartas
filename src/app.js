a9538f688d7393767e0b899192d379791980905b
Alejandro Rivera <124119414+AlejandroRivera2306@users.noreply.github.com>
2023-04-26 21:09:05 +0000

    CAMBIOS ADAPTAR ELEMENTOS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-/* eslint-disable */
+// /* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function loadFunction() {
   let numeroCarta = [
@@ -27,7 +27,11 @@ window.onload = function loadFunction() {
     numeroCarta[Math.floor(Math.random() * numeroCarta.length)];
   document.getElementById("simbolo1").innerHTML = simbolIgual;
   document.getElementById("simbolo2").innerHTML = simbolIgual;
-
+  document.getElementById("tamañoCarta").style.height = "600px";
+  document.getElementById("tamañoCarta").style.width = "400px";
+  document.getElementById("simbolo2").style.fontSize = "50px";
+  document.getElementById("simbolo1").style.fontSize = "50px";
+  document.getElementById("numero").style.fontSize = "70px";
   if (simbolIgual === "♦") {
     document.getElementById("simbolo2").classList.add("rojo");
     document.getElementById("simbolo1").classList.add("rojo");
@@ -77,5 +81,25 @@ window.randomCard = function randomCard() {
     document.getElementById("simbolo2").classList.remove("rojo");
     document.getElementById("simbolo1").classList.remove("rojo");
     document.getElementById("numero").classList.remove("rojo");
+  }
+};
+const intervalID = setInterval(window.randomCard, 3000);
+
+window.obtenerElementos = function obtenerElementos() {
+  let alto = parseInt(document.getElementById("alto").value);
+  let ancho = parseInt(document.getElementById("ancho").value);
+
+  if (alto <= 400 || ancho <= 400) {
+    document.getElementById("tamañoCarta").style.height = "600px";
+    document.getElementById("tamañoCarta").style.width = "400px";
+    alert("LOS VALORES DE LA CARTA NO SON ADECUADOS 400 EN ADELANTE");
+  } else if (alto === 500) {
+    document.getElementById("tamañoCarta").style.height = `${alto}px`;
+    document.getElementById("simbolo2").classList.remove("tamañoR");
+    document.getElementById("tamañoCarta").style.width = `${ancho}px`;
+  } else if (alto > 500) {
+    document.getElementById("tamañoCarta").style.height = `${alto}px`;
+    document.getElementById("simbolo2").classList.add("tamañoR");
+    document.getElementById("tamañoCarta").style.width = `${ancho}px`;
   }
 };
